5bccbdbfa8ae60c0e8eb6d71a5fa74b34a718a28
mvasquezb99 <mvasquezb@eafit.edu.co>
2024-08-29 09:13:48 -0500

    feat:Project init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-center w-full h-fit text-3xl'>Analisis numerico</h1>
+    </>
+  )
+}
+
+export default App
